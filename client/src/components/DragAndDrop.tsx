c9895c961c3216e2b0768beabf78a51f224b5848
Nina Barbakadze <barbakadzeninaa@gmail.com>
2021-11-25 11:18:21 +0100

    feat: drag and drop board created

---
+++ client/src/components/DragAndDrop.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { useDrop } from 'react-dnd';
+
+
+const DragAndDrop = () => {
+  return (
+    <div>
+      <div className="input"> 
+      </div>
+      <div className="board">
+      </div>
+    </div>
+  );
+};
+
+export default DragAndDrop;
